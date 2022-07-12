5c977ed0bb66efad643885003ae7285c91dc4f01
nielsRocholl <nielsrocholl@gmail.com>
2022-07-12 12:48:37 +0200

    clean up

---
+++ Algorithms/multinomial_naive_bayes.py
@@ -0,0 +1,18 @@
+import random
+
+import numpy as np
+
+
+class MultinomialNB:
+    def __init__(self, articles_per_tag):
+        # Don't change the following two lines of code.
+        self.articles_per_tag = articles_per_tag  # See question prompt for details.
+        self.train()
+
+    def train(self):
+        # Write your code here.
+        pass
+
+    def predict(self, article):
+        # Write your code here.
+        pass
